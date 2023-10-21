d96311d1d35f193f409f093bb32888aa80de28d5
Shahzeb Khan <shahzeb001@gmail.com>
2023-10-21 16:16:14 -0700

    Let the code breath + loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 import { useState } from "react";
+import { promptRequest } from "./lib";
 import "./App.css";
+
 function App() {
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
   async function handleGoClick() {
-    const r = await fetch("https://llama.shahzeb001.workers.dev/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ prompt }),
-    });
-    const { response } = await r.json();
+    setLoading(true);
+    const { response } = await promptRequest(prompt);
     setResponse(response);
+    setLoading(false);
   }
+
   return (
     <>
       <textarea
         placeholder="Your prompt here"
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
+        disabled={loading}
       ></textarea>
-      <button onClick={handleGoClick}>Go</button>
+      <button onClick={handleGoClick} disabled={loading}>
+        Go
+      </button>
       {response && <div className="response-zone">{response}</div>}
     </>
   );
--- src/lib.ts
+++ src/lib.ts
@@ -1,13 +1,19 @@
-export async function promptRequest(
-  prompt: string
-): Promise<{ response: string }> {
-  const r = await fetch("https://llama.shahzeb001.workers.dev/", {
+const ENDPOINT = "https://llama.shahzeb001.workers.dev/";
+
+export interface Response {
+  response: string;
+}
+
+export async function promptRequest(prompt: string): Promise<Response> {
+  const r = await fetch(ENDPOINT, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ prompt }),
   });
+
   const j = await r.json();
+
   return j;
 }
