dc6e3f0f25aaef95945e1a493ef0398480860fc8
Shahzeb Khan <shahzeb001@gmail.com>
2023-10-22 19:34:18 -0700

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import "./index.css";
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="gpt-shahzeb-theme">
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
